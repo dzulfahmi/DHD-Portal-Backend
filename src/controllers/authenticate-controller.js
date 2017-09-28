403623a05b07e951f9f27e374e6f50173e3b4490
fitra <fitradzulfahmi@gmail.com>
2017-09-28 17:19:44 +0700

    1 authentication bcrypt

--- src/controllers/authenticate-controller.js
+++ src/controllers/authenticate-controller.js
@@ -1,4 +1,5 @@
 var connection = require('../../config');
+var bcrypt = require('bcrypt');
 
 module.exports.authenticate=function(req, res){
     var email=req.body.email;
@@ -12,18 +13,21 @@ module.exports.authenticate=function(req, res){
             })
         }else{
             if(results.length > 0){
-                if(password==results[0].u_password){
-                    res.json({
-                        status:true,
-                        message:'successfully authenticated'
-                    })
-                }else{
-                    res.json({
-                        status:false,
-                        message:"Email and password does not match"
-                    });
-                }
-            }else{
+                bcrypt.compare(password, results[0].u_password, function(err, ress){
+                    if(!ress){
+                        res.json({
+                            status:false,
+                            message:"Email and password does not match"
+                        })
+                    }else{
+                        res.json({
+                            status:true,
+                            message:"Successfully Login"
+                        })
+                    }
+                });
+            }
+            else{
                 res.json({
                     status:false,
                     message:"Email does not exists"
