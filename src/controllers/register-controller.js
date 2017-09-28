5c8206c32da9d4f601e861be66fe2c30fa5fcc6e
fitra <fitradzulfahmi@gmail.com>
2017-09-28 17:20:00 +0700

    1 register bcrypt

--- src/controllers/register-controller.js
+++ src/controllers/register-controller.js
@@ -1,27 +1,36 @@
 var connection = require('../../config');
+var bcrypt = require('bcrypt');
 
 module.exports.register = function(req, res){
     var today = new Date();
-    var users={
-        "u_name"        : req.body.name,
-        "u_fullname"    : req.body.fullname,
-        "u_email"       : req.body.email,
-        "u_password"    : req.body.password,
-        "u_preference"  : req.body.preference,
-        "u_created_at"  : today
-    }
-    connection.query('INSERT INTO cr_user SET ?', users, function(error, results, field){
-        if (error) {
-            res.json({
-                status:false,
-                message:'there are some error in query'
-            })
+
+    bcrypt.hash(req.body.password, 10, function(err, hash){
+        if(!err){
+            var users={
+                "u_name"        : req.body.name,
+                "u_fullname"    : req.body.fullname,
+                "u_email"       : req.body.email,
+                "u_password"    : hash,
+                "u_preference"  : req.body.preference,
+                "u_created_at"  : today
+            }
+
+            connection.query('INSERT INTO cr_user SET ?', users, function(error, results, field){
+                if (error) {
+                    res.json({
+                        status:false,
+                        message:'there are some error in query'
+                    })
+                }else{
+                    res.json({
+                        status:true,
+                        data:results,
+                        message:'user registered successfully'
+                    })
+                }
+            });
         }else{
-            res.json({
-                status:true,
-                data:results,
-                message:'user registered successfully'
-            })
+            console.log(err)
         }
     });
 }
